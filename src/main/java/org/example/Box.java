ed10ed957c28e7014ba3393205743d4feee48225
Yaroslav Voitiuk <yaroslav.voitiuk@gmail.com>
2021-10-20 14:04:29 +0300

    task fixed

--- src/main/java/org/example/Box.java
+++ src/main/java/org/example/Box.java
@@ -1,23 +1,33 @@
 package org.example;
 
 public class Box {
-     private volatile boolean isOpen;
-     private static final int SLEEP = 2000;
+    private volatile boolean isOpen;
+    private static final int SLEEP = 2000;
+    private static final int CYCLES_QUAN = 3;
+    private int currCycleVal = 0;
 
-     public void openBox() {
-          try {
-               Thread.sleep(SLEEP);
-               System.out.println("Box is open");
-               isOpen = true;
-          } catch (InterruptedException e) {
-               e.printStackTrace();
-          }
-     }
 
-     public void closeBox() {
-          while (isOpen) {
-               System.out.println("Box is closed");
-               isOpen = false;
-          }
-     }
+    public void openBox() {
+        try {
+            while (currCycleVal != CYCLES_QUAN) {
+                if (!isOpen) {
+                    isOpen = true;
+                    System.out.println("Box is open");
+                    currCycleVal++;
+                    Thread.sleep(SLEEP);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeBox() {
+        while (currCycleVal != 3) {
+            if (isOpen) {
+                isOpen = false;
+                System.out.println("Box is closed");
+            }
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,19 +2,18 @@ package org.example;
 
 
 public class Main {
-    public static final int CYCLE_QUANT = 3;
-    public static void main( String[] args ) throws InterruptedException {
+
+    public static void main(String[] args) throws InterruptedException {
         Box box = new Box();
 
-        for (int i = 0; i < CYCLE_QUANT; i++){
-            Thread thread1 = new Thread(box::openBox,"user");
-            Thread thread2 = new Thread(box::closeBox,"box");
-            thread1.start();
-            thread1.join();
-            thread2.start();
-            thread2.join();
-        }
+        Thread thread1 = new Thread(box::openBox, "user");
+        Thread thread2 = new Thread(box::closeBox, "box");
 
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
 
     }
 }
